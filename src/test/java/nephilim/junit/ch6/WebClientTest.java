7ec8db372a28e73b2428b200b2bf4a691c203639
dongwook lee <i015005@gmail.com>
2011-07-19 17:17:24 +0900

    6장: 서버의 자원 반환 실패 시 테스트 추가

--- src/test/java/nephilim/junit/ch6/WebClientTest.java
+++ src/test/java/nephilim/junit/ch6/WebClientTest.java
@@ -1,11 +1,11 @@
 package nephilim.junit.ch6;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 import java.net.URL;
 
+import nephilim.junit.ch6.handler.ContentNotFoundHandler;
 import nephilim.junit.ch6.handler.StaticMessageReturnHandler;
 
 import org.junit.AfterClass;
@@ -22,10 +22,17 @@ public class WebClientTest {
 	@BeforeClass
 	public static void startUpServer() throws Exception {
 		server = new Server(8080);
-		Context context = new Context(server, "/test-okay");
-		StaticMessageReturnHandler handler = new StaticMessageReturnHandler();
-		handler.setMessage(MESSAGE);
-		context.setHandler(handler);
+		
+		// 정상 반환 테스트용 context 설정 
+		Context okContext = new Context(server, "/test-okay");
+		StaticMessageReturnHandler msgReturnHandler = new StaticMessageReturnHandler();
+		msgReturnHandler.setMessage(MESSAGE);
+		okContext.setHandler(msgReturnHandler);
+		
+		// 비정상 반환 테스트용 context 설정 
+		Context errorContext = new Context(server, "/test-error");
+		errorContext.setHandler(new ContentNotFoundHandler());
+		
 		server.start();
 	}
 	
@@ -42,8 +49,18 @@ public class WebClientTest {
 		String result = client.getContent(okUrl);
 		
 		assertThat(
-				"okUrl의 응답 내용은 지정된 메세지와 같아야 한다",
+				"ok url의 응답 내용은 지정된 메세지와 같아야 한다",
 				result, is(equalTo(MESSAGE)));
 	}
 
+	@Test
+	public void testGetContent_Error() throws Exception{
+		WebClient client = new WebClient();
+		URL errorUrl = new URL("http://localhost:8080/test-error");
+		String result = client.getContent(errorUrl);
+		
+		assertThat(
+				"error url의 응답 내용은 null이어야 한다",
+				result, is(nullValue()));
+	}
 }
---
+++ src/test/java/nephilim/junit/ch6/handler/ContentNotFoundHandler.java
@@ -0,0 +1,18 @@
+package nephilim.junit.ch6.handler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.jetty.handler.AbstractHandler;
+
+public class ContentNotFoundHandler extends AbstractHandler {
+	public void handle(String target, HttpServletRequest request,
+			HttpServletResponse response, int dispatch) throws IOException,
+			ServletException {
+		response.sendError(HttpServletResponse.SC_NOT_FOUND);
+	}
+
+}
